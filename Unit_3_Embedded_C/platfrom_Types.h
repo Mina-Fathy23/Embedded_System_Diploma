9099d21244ccf3494628c3ae1fbc6faf3289131c
Mina-Fathy23 <minafathy2013738@gmial.com>
2024-08-08 15:01:10 +0300

    added spaces

--- Unit_3_Embedded_C/platfrom_Types.h
+++ Unit_3_Embedded_C/platfrom_Types.h
@@ -12,9 +12,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define CPU_TYPE			  CPU_TYPE_32
+#define CPU_TYPE			CPU_TYPE_32
 #define CPU_BIT_ORDER		MSB_FIRST
-#define	CPU_BYTE_ORDER	HIGH_BYTE_FIRST
+#define	CPU_BYTE_ORDER		HIGH_BYTE_FIRST
 
 #ifndef FALSE
 #define FALSE		(boolean)false
@@ -24,10 +24,10 @@
 #define TRUE		(boolean)true
 #endif
 
-typedef	_Bool				  boolean;
+typedef	_Bool				boolean;
 typedef	int8_t				sint8;
 typedef uint8_t				uint8;
-typedef char				  char_t;
+typedef char				char_t;
 typedef int16_t				sint16;
 typedef uint16_t			uint16;
 typedef int32_t				sint32;
