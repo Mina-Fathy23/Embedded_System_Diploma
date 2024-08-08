35ee2dbf770fb7bac0003bc09759b49bb37f878a
Mina Fathy <110775882+Mina-Fathy23@users.noreply.github.com>
2024-08-08 14:59:33 +0300

    Update platfrom_Types.h

--- Unit_3_Embedded_C/platfrom_Types.h
+++ Unit_3_Embedded_C/platfrom_Types.h
@@ -12,11 +12,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define CPU_TYPE			CPU_TYPE_32
+#define CPU_TYPE			  CPU_TYPE_32
 #define CPU_BIT_ORDER		MSB_FIRST
-#define	CPU_BYTE_ORDER		HIGH_BYTE_FIRST
+#define	CPU_BYTE_ORDER	HIGH_BYTE_FIRST
 
-#ifndef FASLE
+#ifndef FALSE
 #define FALSE		(boolean)false
 #endif
 
@@ -24,10 +24,10 @@
 #define TRUE		(boolean)true
 #endif
 
-typedef	_Bool				boolean;
+typedef	_Bool				  boolean;
 typedef	int8_t				sint8;
 typedef uint8_t				uint8;
-typedef char				char_t;
+typedef char				  char_t;
 typedef int16_t				sint16;
 typedef uint16_t			uint16;
 typedef int32_t				sint32;
