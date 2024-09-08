5e38c357bfa38ce760f151a87af4c817dc14e0a7
Mina-Fathy23 <minafathy2013738@gmial.com>
2024-09-08 16:04:05 +0300

    fixed big with INT1 & INT0

--- Unit_6_MCU_Fundamentals/Lesson_4/ATMega32_Interrupt_Lab/main.c
+++ Unit_6_MCU_Fundamentals/Lesson_4/ATMega32_Interrupt_Lab/main.c
@@ -2,7 +2,7 @@
  * main.c
  *
  *  Created on: Sep 7, 2024
- *      Author: EliteBook
+ *      Author: Mina Fathy
  */
 
 
@@ -14,7 +14,11 @@
 #define IO_Base 0x20
 
 //GDPIO_Port D
-#define DDRD 	*((vuint32_t *)(IO_Base + 0x11))
+#define DDRB 	*((vuint32_t*)(IO_Base + 0x17))
+#define PORTB 	*((vuint32_t*)(IO_Base + 0x18))
+
+//GDPIO_Port D
+#define DDRD 	*((vuint32_t*)(IO_Base + 0x11))
 #define PORTD 	*((vuint32_t*)(IO_Base + 0x12))
 
 
@@ -28,8 +32,8 @@
 
 void GPIO_init()
 {
-	DDRD  |= 0b11100000;
-	DDRB  |= 0 << 2;
+	DDRD |= 0b11110000;
+	DDRB |= (0 << 2);
 }
 
 void EXTI_init()
@@ -38,16 +42,19 @@ void EXTI_init()
 	MCUCR |= (0b01 << 0);
 
 	//	Set INT1 to Rising edge
-	MCUCR |= (0b11 << 3);
+	MCUCR |= (1 << 3);
+	MCUCR |= (1 << 2);
 
 	//	Set INT2 to Falling edge
 	MCUCSR |= (0 << 6);
 
+	//	Enable INT0, INT1, INT2
+	GICR |= (1 << 7);
+	GICR |= (1 << 6);
+	GICR |= (1 << 5);
+
 	//	Enable Global Interrupt
 	SREG |= (1 << 7);
-
-	//	Enable INT0, INT1, INT2
-	GICR |= (0b111 << 7);
 
 }
 
@@ -58,9 +65,13 @@ int main()
 
 	while(1)
 	{
-		PORTD |= (0 << 5);
-		PORTD |= (0 << 6);
-		PORTD |= (0 << 7);
+		PORTD |= (1 << 4);
+		_delay_ms(200);
+		PORTD &= (0 << 4);
+		PORTD &= (0 << 5);
+		PORTD &= (0 << 6);
+		PORTD &= (0 << 7);
+		_delay_ms(200);
 	}
 	return 1;
 }
@@ -69,16 +80,19 @@ ISR(INT0_vect)
 {
 	PORTD |= (1 << 5);
 	_delay_ms(1000);
+	GIFR |= (1 << 6);
 }
 
 ISR(INT1_vect)
 {
-	PORTD |= (1 << 7);
+	PORTD |= (1 << 6);
 	_delay_ms(1000);
+	GIFR |= (1 << 7);
 }
 
 ISR(INT2_vect)
 {
-	PORTD |= (1 << 6);
+	PORTD |= (1 << 7);
 	_delay_ms(1000);
+	GIFR |= (1 << 5);
 }
