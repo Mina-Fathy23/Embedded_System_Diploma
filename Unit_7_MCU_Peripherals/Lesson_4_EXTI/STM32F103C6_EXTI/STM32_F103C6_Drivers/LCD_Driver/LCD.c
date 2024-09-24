a6b86f05bff434984f109f2ced26be592e53fbf2
Mina-Fathy23 <minafathy2013738@gmial.com>
2024-09-24 16:10:22 +0300

    changed LCD.c

--- Unit_7_MCU_Peripherals/Lesson_4_EXTI/STM32F103C6_EXTI/STM32_F103C6_Drivers/LCD_Driver/LCD.c
+++ Unit_7_MCU_Peripherals/Lesson_4_EXTI/STM32F103C6_EXTI/STM32_F103C6_Drivers/LCD_Driver/LCD.c
@@ -1,359 +1,115 @@
 /*
- * LCD.c
+ * keypad.c
  *
  *  Created on: Sep 12, 2024
  *      Author: Mina Fathy
  */
 
-#include "LCD.h"
+#include "keypad.h"
 
-GPIO_PinConfig_t PinCfg;
 
-/*  RS & RW Values from LCD SPECS
-	RS
-	 0:  (Command Mode) Instruction register (for write) Busy flag
-	 1: Data register (for write and read)
-
-	RW
-	 Selects read or write.
-	 0:Write
-	 1:Read
-
-	E
-	1:Starts data read/write.
- */
-
+char keypad_character[4][4] = {
+		{'7', '8', '9', '/'},
+		{'4', '5', '6', 'X'},
+		{'1', '2', '3', '-'},
+		{'C', '0', '=', '+'}  };
 
 /**================================================================
- * @Fn				- LCD_KICK
- * @breif			- Makes the LCD Display the input Char/Command
+ * @Fn				- KEYPAD_Init
+ * @breif			- Initializes Keypads' Port
  * @param [in]		- None
  * @retval			- None
  * Note				- None
  */
-void LCD_KICK(void)
-
+void KEYPAD_Init()
 {
-	//ClearBit(LCD_CTRL, EN_Switch);
-	MCAL_GPIO_WritePin(LCD_CTRL, EN_SWITCH, GPIO_PIN_SET);
-	wait(20); //Delay Between Writing Each Letter
-	//SetBit(LCD_CTRL, EN_Switch);
-	MCAL_GPIO_WritePin(LCD_CTRL, EN_SWITCH, GPIO_PIN_RESET);
+	/*	KEYPAD_DataDir &= ~((1 << R0) | (1 << R1) | (1 << R2) | (1 << R3));
+	KEYPAD_DataDir |=  ((1 << C0) | (1 << C1) | (1 << C2) | (1 << C3));
+	KEYPAD_PORT = 0xFF;
+	 */
+
+	GPIO_PinConfig_t PinCfg;
+
+
+	PinCfg.GPIO_PinNumber = R0;
+	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_PD;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = R1;
+	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_PD;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = R2;
+	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_PD;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = R3;
+	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_PD;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+
+	//=========================================================
+
+	//Set C0 >> C3 as Output Push-Pull
+	PinCfg.GPIO_PinNumber = C0;
+	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
+	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = C1;
+	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
+	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = C2;
+	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
+	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+	PinCfg.GPIO_PinNumber = C3;
+	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
+	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
+	MCAL_GPIO_Init(KEYPAD_PORT, &PinCfg);
+
+
 
 }
 
 /**================================================================
- * @Fn				- LCD_check_lcd_isbusy
- * @breif			- Checks if the LCD is Busy/free to execute command
+ * @Fn				- KEYPAD_GetChar
+ * @breif			- Checks for the pressed button
  * @param [in]		- None
- * @retval			- None
+ * @retval			- the pressed button as ASCII Code char
  * Note				- None
  */
-void LCD_check_lcd_isbusy(void)
+char KEYPAD_GetChar()
 {
-	//Read Mode
-	//LCD_DataDir_PORT &= ~(0xFF << DATA_shift);
+	vuint8_t keypad_C[4] = {C0, C1, C2, GPIO_PIN_8}; //columns
+	vuint8_t keypad_R[4] = {R0, R1, R2, R3}; //rows of the keypad
 
-	//Set PIN 0 >> 7 Set as Input Floating Point
-	PinCfg.GPIO_PinNumber = GPIO_PIN_0;
-	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_FLO;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
+	vuint8_t i, j;
 
-	PinCfg.GPIO_PinNumber = GPIO_PIN_1;
-	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_FLO;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_2;
-	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_FLO;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_3;
-	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_FLO;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_4;
-	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_FLO;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_5;
-	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_FLO;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_6;
-	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_FLO;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_7;
-	PinCfg.GPIO_Mode = GPIO_MODE_INPUT_FLO;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-
-	//Instruction Mode
-	//SetBit(LCD_CTRL->, RW_SWITCH);
-	//ClearBit(LCD_CTRL, RS_SWITCH);
-
-	MCAL_GPIO_WritePin(LCD_CTRL, RW_SWITCH, GPIO_PIN_SET);
-	MCAL_GPIO_WritePin(LCD_CTRL, RS_SWITCH, GPIO_PIN_RESET);
-
-	LCD_KICK();
-
-	//Write Mode
-	//	LCD_DataDir_PORT = 0xFF;
-	//	SetBit(LCD_CTRL, RS_SWITCH);
-	//	ClearBit(LCD_CTRL, RW_SWITCH);
-	MCAL_GPIO_WritePin(LCD_CTRL, RW_SWITCH, GPIO_PIN_RESET);
-
-}
-
-/**================================================================
- * @Fn				- LCD_clear_screen
- * @breif			- Clears Screen from any characters and moves cursor to Line: 1 Position: 0
- * @param [in]		- None
- * @retval			- None
- * Note				- None
- */
-void LCD_clear_screen(void)
-{
-	LCD_WRITE_COMMAND(LCD_CLEAR_SCREEN);
-}
-
-/**================================================================
- * @Fn				- LCD_clear_screen
- * @breif			- Initializes LCD's Port
- * @param [in]		- None
- * @retval			- None
- * Note				- None
- */
-void LCD_INIT()
-{
-	wait(20);
-	//Write Mode
-	//	LCD_DataDir_CTRL |= (1 << EN_Switch) | (1 << RW_SWITCH) | (1 << RS_SWITCH);
-	//	LCD_CTRL &= ~((1 << EN_Switch) | (1 << RW_SWITCH) | (1 << RS_SWITCH));
-
-	//EN_Switch, RW_SWITCH, RS_SWITCH Set as Output Push-Pull
-	PinCfg.GPIO_PinNumber = RS_SWITCH;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = RW_SWITCH;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = EN_SWITCH;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	//=====================================================
-	//Set PIN 0 >> 7 Set as Output Push-Pull
-	//LCD_DataDir_PORT = 0xFF;
-	PinCfg.GPIO_PinNumber = GPIO_PIN_0;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_1;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_2;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_3;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_4;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_5;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_6;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	PinCfg.GPIO_PinNumber = GPIO_PIN_7;
-	PinCfg.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinCfg.GPIO_Output_Speed = GPIO_SPEED_10MHz;
-	MCAL_GPIO_Init(LCD_CTRL, &PinCfg);
-
-	//============================================================
-
-	MCAL_GPIO_WritePin(LCD_CTRL, EN_SWITCH, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCD_CTRL, RS_SWITCH, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCD_CTRL, RW_SWITCH, GPIO_PIN_RESET);
-
-	//=======================================================
-	wait(15);
-	LCD_clear_screen();
-
-#ifdef EIGHT_BIT_MODE
-	LCD_WRITE_COMMAND(LCD_FUNCTION_8BIT_2LINES);
-#endif
-#ifdef FOUR_BIT_MODE
-	LCD_WRITE_COMMAND(0x02); //command to tell LCD we are using D4 -> D7
-	LCD_WRITE_COMMAND(LCD_FUNCTION_4BIT_2LINES);
-#endif
-
-	LCD_WRITE_COMMAND(LCD_ENTRY_MODE);
-	LCD_WRITE_COMMAND(LCD_BEGIN_AT_FIRST_ROW);
-	LCD_WRITE_COMMAND(LCD_DISP_ON_CURSOR_BLINK);
-}
-
-/**================================================================
- * @Fn				- LCD_WRITE_COMMAND
- * @breif			- Executes a Command from LCD's Functionality
- * @param [in]		- command: Specifies the Command code according to @ref LCD_define
- * @retval			- None
- * Note				- None
- */
-void LCD_WRITE_COMMAND(vuint8_t command)
-{
-	//	LCD_check_lcd_isbusy();
-#ifdef EIGHT_BIT_MODE
-	//	LCD_CTRL  = command;
-	//	LCD_CTRL &= ~((1 << RW_SWITCH) | (1 << RS_SWITCH));
-	MCAL_GPIO_WritePort(LCD_CTRL, command);
-	MCAL_GPIO_WritePin(LCD_CTRL, RW_SWITCH, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCD_CTRL, RS_SWITCH, GPIO_PIN_RESET);
-
-	wait(1);
-	LCD_KICK();
-#endif
-#ifdef FOUR_BIT_MODE
-	//First Stage
-	LCD_CTRL  = (LCD_CTRL  & 0x0F) | (command & 0xF0);
-	LCD_CTRL &= ~((1 << RW_SWITCH) | (1 << RS_SWITCH));
-	wait(10);
-	LCD_KICK();
-
-	//Second Stage
-	LCD_CTRL  = (LCD_CTRL  & 0x0F) | (command << DATA_shift);
-	LCD_CTRL &= ~((1 << RW_SWITCH) | (1 << RS_SWITCH));
-	wait(10);
-	LCD_KICK();
-#endif
-}
-
-/**================================================================
- * @Fn				- LCD_WRITE_CHAR
- * @breif			- Writes ASCII Characters on LCD
- * @param [in]		- character: Specifies the character cod be written on LCD
- * @retval			- None
- * Note				- None
- */
-void LCD_WRITE_CHAR(vuint8_t character)
-{
-	//  LCD_check_lcd_isbusy();
-#ifdef EIGHT_BIT_MODE
-	//	LCD_CTRL  = character;
-	//	SetBit(LCD_CTRL, RS_SWITCH);
-	//	ClearBit(LCD_CTRL, RW_SWITCH);
-	MCAL_GPIO_WritePort(LCD_CTRL , character);
-	MCAL_GPIO_WritePin(LCD_CTRL, RW_SWITCH, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCD_CTRL, RS_SWITCH, GPIO_PIN_SET);
-
-	wait(1);
-	LCD_KICK();
-#endif
-#ifdef FOUR_BIT_MODE
-	//First Stage
-	LCD_CTRL  = (LCD_CTRL  & 0x0F) | (character & 0xF0);
-	SetBit(LCD_CTRL, RS_SWITCH);
-	ClearBit(LCD_CTRL, RW_SWITCH);
-	wait(10);
-	LCD_KICK();
-
-	//Second Stage
-	LCD_CTRL  = (LCD_CTRL  & 0x0F) | (character << DATA_shift);
-	SetBit(LCD_CTRL, RS_SWITCH);
-	ClearBit(LCD_CTRL, RW_SWITCH);
-	wait(10);
-	LCD_KICK();
-#endif
-}
-/**================================================================
- * @Fn				- LCD_WRITE_STRING
- * @breif			- Writes a Senctence on LCD
- * @param [in]		- string: Specifies the string to be written on LCD
- * @retval			- None
- * Note				- None
- */
-void LCD_WRITE_STRING(char* string)
-{
-	vuint8_t count = 0;
-	while(*string > 0)
+	for(i = 0; i < 4; i++)
 	{
-		LCD_WRITE_CHAR(*(string)++);
-		count ++;
-		if(count == 16)
+
+		// Send a high to a particular column of the keypad
+		MCAL_GPIO_WritePin(KEYPAD_PORT, keypad_C[i], GPIO_PIN_SET);
+
+		for(j = 0; j < 4; j++)
 		{
-			LCD_GOTO_XY(2, 0);
+			if(MCAL_GPIO_ReadPin(KEYPAD_PORT, keypad_R[j])) // Check if is key is pressed
+			{
+				while(MCAL_GPIO_ReadPin(KEYPAD_PORT, keypad_R[j])) // Wait for the key to be released (Single pressed)
+				{
+					//Will Still pressed DO Noting
+				}
+				return keypad_character[j][i];
+			}
 		}
-		else if(count == 32)
-		{
-			//wait(); add delay here before Clearing Screen
-			LCD_clear_screen();
-			LCD_GOTO_XY(1, 0);
-			count = 0;
-		}
+		wait(10);
+		MCAL_GPIO_WritePin(KEYPAD_PORT, keypad_C[i], GPIO_PIN_RESET);//Return the column low again
 	}
+	return 'N';
 }
 
-/**================================================================
- * @Fn				- LCD_GOTO_XY
- * @breif			- Moves cursor to Sepcific location
- * @param [in]		- line: Specifies the line to be written on LCD can be (1 0R 2)
- * @param [in]		- position : Specifies the position to be written on LCD can be from the right boarder of screen
- * 								 value [0:16]
- * @retval			- None
- * Note				- None
- */
-void LCD_GOTO_XY(vuint8_t line, vuint8_t position)
-{
-	if(line == 1)
-	{
-		LCD_WRITE_COMMAND(LCD_BEGIN_AT_FIRST_ROW + position);
-	}
-	else if(line == 2)
-	{
-		LCD_WRITE_COMMAND(LCD_BEGIN_AT_SECOND_ROW + position);
-	}
-}
-
-/**================================================================
- * @Fn				- LCD_CUSTOM_CHAR
- * @breif			- adds a Custom character to LCD SRAM
- * @param [in]		- None
- * @retval			- None
- * Note				- None
- */
-void LCD_CUSTOM_CHAR(void)
-{
-	LCD_WRITE_COMMAND(64);
-	LCD_WRITE_CHAR(0);
-	LCD_WRITE_CHAR(14);
-	LCD_WRITE_CHAR(17);
-	LCD_WRITE_CHAR(2);
-	LCD_WRITE_CHAR(4);
-	LCD_WRITE_CHAR(4);
-	LCD_WRITE_CHAR(0);
-	LCD_WRITE_CHAR(4);
-	LCD_WRITE_COMMAND(LCD_BEGIN_AT_FIRST_ROW);
-	LCD_WRITE_CHAR(0);
-	wait(10);
-}
 
